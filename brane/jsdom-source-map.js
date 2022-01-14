a35959637ce3bc4da1bc078ed0c0d58cd4702f4b
Seo Sanghyeon <sanxiyn@gmail.com>
2022-01-14 19:42:03 +0900

    Import from jsdom 19.0.0

--- brane/jsdom-source-map.js
+++ brane/jsdom-source-map.js
@@ -1,1 +1,7 @@
-export default [];
+export default [
+  ['scripts/webidl/convert.js', 'convert-idl.js'],
+  ['lib/jsdom/living/window/History.webidl', 'idl/History.webidl'],
+  ['lib/jsdom/living/window/Location.webidl', 'idl/Location.webidl'],
+  ['lib/jsdom/living/window/History-impl.js', 'impl/History-impl.js'],
+  ['lib/jsdom/living/window/Location-impl.js', 'impl/Location-impl.js']
+];
